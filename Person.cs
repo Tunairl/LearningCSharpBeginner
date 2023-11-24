58084fed05a99aae607b5327790105ba0ebc0bee
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-11-24 23:04:17 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿/*namespace CSharpFundamentals
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hello! My name is " + firstName + " " + lastName + ".");
+        }
+    }
+}*/
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seanson = Season.Summer;
+
+            switch (seanson) {
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("Sale is available.");
+                    break;
+
+                case Season.Spring:
+                    Console.WriteLine("It's spring time.");
+                    break;
+
+                case Season.Winter:
+                    Console.WriteLine("It's winter time.");
+                    break;
+
+                default: 
+                    Console.WriteLine("I don't understand that season.");
+                    break;
+            }
+
+
+        }
+
+        
+
+    }
+}
