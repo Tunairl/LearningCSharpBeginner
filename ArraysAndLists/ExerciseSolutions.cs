5176b219e5ce75e9016250aafc75f35489909a2f
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-11-28 01:48:04 +0300

    Add files via upload

---
+++ ArraysAndLists/ExerciseSolutions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    internal class ExerciseSolutions
+    {
+        static void Main(string[] args)
+        {
+            Exercise1();
+            Exercise2();
+            Exercise3();
+            Exercise4();
+        }
+
+        public static void Exercise1()
+        {
+            var friendsList = new List<string>();
+
+            Console.WriteLine("Enter a name.");
+            string nameInput = Console.ReadLine();
+
+            while (!String.IsNullOrEmpty(nameInput))
+            {
+                friendsList.Add(nameInput);
+
+                switch (friendsList.Count)
+                {
+                    case 0:
+
+                        Console.WriteLine();
+                        break;
+
+                    case (1):
+
+                        Console.WriteLine($"{friendsList[0]} liked your post.");
+                        break;
+
+
+                    case (2):
+
+                        Console.WriteLine($"{friendsList[0]} and {friendsList[1]} liked your post.");
+                        break;
+
+                    default:
+
+                        Console.WriteLine($"{friendsList[0]}, {friendsList[1]} and {friendsList.Count - 2} other(s) liked your post.");
+                        break;
+                }
+                nameInput = Console.ReadLine();
+            }
+        }
+
+        public static void Exercise2()
+        {
+            Console.WriteLine("Enter your name. I'll reverse it.");
+            string name = Console.ReadLine();
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i]);
+            }
+
+            //name.ToCharArray();
+            //var reversedName = name.Reverse();
+
+            //foreach (char c in reversedName)
+            //{
+            //    Console.Write(c);
+            //}
+        }
+
+        public static void Exercise3()
+        {
+            Console.WriteLine("Enter 5 different numbers.");
+            var numbersList = new List<int>();
+
+            while (numbersList.Count < 5)
+            {
+                var userInput = Int32.Parse(Console.ReadLine());
+
+                if (numbersList.Contains(userInput))
+                {
+                    Console.WriteLine("Enter a unique number. Try again.");
+                    continue;
+                }
+                numbersList.Add(userInput);
+            }
+
+            numbersList.Sort();
+            Console.WriteLine();
+
+            foreach (int i in numbersList)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        public static void Exercise4()
+        {
+            var numbersList = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number.");
+                var userInput = Console.ReadLine();
+
+                if (!Int32.TryParse(userInput, out int number))
+                {
+                    break;
+                }
+                numbersList.Add(number);
+            }
+            foreach (int i in numbersList.Distinct())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ArraysAndLists/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 6, 7, 13, 4, 9, 2, 1 };
+
+            //Length
+            Console.WriteLine("Length: " + numbers.Length);
+            Console.WriteLine(numbers[4]);
+
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            //Clear()
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Copy()
+            int[] another = new int[3];
+
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Sort()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Reverse()
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+
+        }
+    }
+}
