2cb41f6b624a9676535d556804b38cf7dcebaf64
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-11-29 16:22:33 +0300

    Add files via upload

---
+++ WorkingWithText/LiveCoding.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using static System.Net.Mime.MediaTypeNames;
+
+//namespace WorkingWithText
+//{
+//    internal class LiveCoding
+//    {
+//        static void Main(string[] args)
+//        {
+//            var text = "This is a really very so much many grand great and fretless jazzy.";
+//            var summary = StringUtility.SummarizeText(text, 25);
+//            Console.WriteLine(summary);
+//        }
+        
+//    }
+//}
